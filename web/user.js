51aa43952776836669923693bb4f98c551fb6e85
ParadoxV5 <paradox.ver5@gmail.com>
2021-07-10 21:14:41 -0600

    Implemented rest of JS: Autopopulate input fields and disabling/Enabling email input

--- web/user.js
+++ web/user.js
@@ -1,13 +1,22 @@
 function newRadio() {
   document.getElementById("existing").style.display = "none";
-  document.getElementById("new").style.display = null; // Revert
+  document.getElementById("new").style.display = null; // Revert hide
+  document.getElementById("email").disabled = false;
 }
 
 function existingRadio(radio) {
+  document.getElementById("email").disabled = true;
   document.getElementById("new").style.display = "none";
-  document.getElementById("existing").style.display = null; // Revert
   
-  //TODO: radio
+  var tds = radio.parentElement.parentElement.children; // radio ➡ td ➡ tr ➡ td[]
+  document.getElementById("email").value = tds[1].innerText;
+  document.getElementById("active").checked = tds[2].innerText != "❎";
+  document.getElementById("first_name").value = tds[3].innerText;
+  document.getElementById("last_name").value = tds[4].innerText;
+  document.getElementById("password").value = tds[5].title;
+  document.getElementById("role").value = tds[5].innerText;
+  
+  document.getElementById("existing").style.display = null; // Revert hide
 }
 
 document.onload = function() {
