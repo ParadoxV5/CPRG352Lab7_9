cce6041bdfb405751991db8c07e8612ddfbb87e8
ParadoxV5 <paradox.ver5@gmail.com>
2021-07-13 01:38:10 -0600

    Update UserAPI.java

--- src/java/service/UserAPI.java
+++ src/java/service/UserAPI.java
@@ -1,29 +1,28 @@
 package service;
 import model.User;
-import service.exception.ServerException;
-public class UserAPI implements java.util.function.Supplier<java.util.LinkedList<User>> {
+import service.exception.*;
+public class UserAPI implements java.util.function.Supplier<java.util.Set<User>> {
   protected final data.UserDB database;
   public UserAPI() throws ServerException { try {
     database = new data.UserDB();
   } catch(Throwable cause) { throw new ServerException(cause); } }
   
   /** R */
-  @Override public java.util.LinkedList<User> get() throws ServerException {
+  @Override public java.util.Set<User> get() throws ServerException {
     try { return database.getAll(); }
     catch(Throwable cause) { throw new ServerException(cause); }
   }
   
   //IDEA: Optimize by detecting modified fields and only query those to the API
-  public void post(String action, String email, String active, String first_name, String last_name, String password, String role) throws service.exception.ServiceException {
-    boolean isValid = false;
-    try {
-      if(action.equalsIgnoreCase("delete")) isValid = database.remove(email); // D
-      else {
-        User user = new User(email, active != null, first_name, last_name, password, model.Role.valueOf(role));
-        if(action.equalsIgnoreCase("create")) isValid = database.add(user); // C
-        else if(action.equalsIgnoreCase("update")) isValid = database.set(user); // U
-      }
-    } catch(Throwable cause) { throw new ServerException(cause); }
-    if(!isValid) throw new service.exception.ClientException();
+  public void post(String action, String email, String active, String first_name, String last_name, String password, String role) throws ServiceException {
+    User user = new User(email, active != null, first_name, last_name, password, model.Role.valueOf(role));
+    try { switch(action.toLowerCase()) {
+      case "create": database.add(user); break; // C
+      case "update": database.update(user); break; // U
+      case "delete": database.remove(user); break; //D
+      default: throw new ClientException();
+    } } catch(javax.persistence.EntityExistsException __) { throw new ClientException(); }
+        catch(ClientException e) { throw e; } // Ignore and re-throw
+        catch(Throwable cause) { throw new ServerException(cause); } // All other exceptions are considered Serverr problems by default
   }
 }
