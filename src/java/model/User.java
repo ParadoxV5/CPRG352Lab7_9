dc6e115cfd48c8dbc1360f2816cab115976d5e42
ParadoxV5 <paradox.ver5@gmail.com>
2021-07-13 03:57:32 -0600

    Renamed `User` getters and setters to use `lower_snake_case`
    To match that SQL column names are in `lower_snake_case` – Looks better mixing with them, too
    [Begun preparing for Lab 9 release]

--- src/java/model/User.java
+++ src/java/model/User.java
@@ -9,36 +9,34 @@ import javax.persistence.*;
   
   // Primary Key
   @Basic(optional=false) @Id @Column(name="email") private String email;
-  public String getEmail() { return email; }
+  public String get_email() { return email; }
   @Override protected String primaryKey() { return email; }
   
   @Basic(optional=false) @Column private boolean active = true;
-  public boolean isActive() { return active; }
-  public void setActive(boolean active) { this.active = active; }
+  public boolean is_active() { return active; }
+  public void set_active(boolean active) { this.active = active; }
   
   @Basic(optional=false) @Column private String first_name, last_name, password;
-  public String getFirst_name() { return first_name; }
-  public void setFirst_name(String first_name) { this.first_name = requireNonNull(first_name); }
-  public String getLast_name() { return last_name; }
-  public void setLast_name(String last_name) { this.last_name = requireNonNull(last_name); }
-  public String getPassword() { return password; }
-  public void setPassword(String password) { this.password = requireNonNull(password); }
+  public String get_first_name() { return first_name; }
+  public void set_first_name(String first_name) { this.first_name = requireNonNull(first_name); }
+  public String get_last_name() { return last_name; }
+  public void set_last_name(String last_name) { this.last_name = requireNonNull(last_name); }
+  public String get_password() { return password; }
+  public void set_password(String password) { this.password = requireNonNull(password); }
   
   @Basic(optional=false) @Column @Enumerated(value=EnumType.STRING) private Role role;
-  public Role getRole() { return role; }
-  public void setRole(Role role) { this.role = requireNonNull(role); }
+  public Role get_role() { return role; }
+  public void set_role(Role role) { this.role = requireNonNull(role); }
   
   // Constructor //
-  public User() {
-    this("", true, "", "", "", Role.REGULAR_USER);
-  }
+  public User() { this("", true, "", "", "", Role.REGULAR_USER); }
   
   public User(String email, boolean active, String first_name, String last_name, String password, Role role) {
     this.email = requireNonNull(email);
-    setActive(active);
-    setFirst_name(first_name);
-    setLast_name(last_name);
-    setPassword(password);
-    setRole(role);
+    set_active(active);
+    set_first_name(first_name);
+    set_last_name(last_name);
+    set_password(password);
+    set_role(role);
   }
 }
